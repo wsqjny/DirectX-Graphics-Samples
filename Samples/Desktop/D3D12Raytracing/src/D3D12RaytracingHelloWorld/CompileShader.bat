dca4d473d19e443a82a3d8ad5cb09f1cee4b296d
NVIDIA.COM\Jinnanq <jinnanq@nvidia.com>
2025-07-08 10:21:27 +0800

    compile shader use dxc.

---
+++ Samples/Desktop/D3D12Raytracing/src/D3D12RaytracingHelloWorld/CompileShader.bat
@@ -0,0 +1,1 @@
+C:\Users\jinnanq.NVIDIA.COM\Downloads\dxc_2025_05_24\bin\x64\dxc.exe Raytracing.hlsl /T lib_6_5 /E MyClosestHitShader /Fo ClosestHit.dxil /Fh ClosestHit.hlsl.h /Vn g_pClosestHitShader /nologo
